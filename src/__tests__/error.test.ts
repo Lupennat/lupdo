157bc4830489287e049d6817c14822d0005f59cb
Claudio Pennati <claudio.pennati@gmail.com>
2023-01-11 09:59:44 +0100

    improve get message from AggregateError (#24)

--- src/__tests__/error.test.ts
+++ src/__tests__/error.test.ts
@@ -1,6 +1,12 @@
 import { PdoError } from '../errors';
 
 describe('Pdo Error', () => {
+    class AggregateErrorFake extends Error {
+        constructor(public errors: Error[], message?: string) {
+            super(message);
+        }
+    }
+
     it('Throws Error With Cause', () => {
         const error = new Error('test');
         let pdoError = new PdoError('Message');
@@ -14,5 +20,13 @@ describe('Pdo Error', () => {
         expect(pdoError.cause).toEqual(error);
         pdoError = new PdoError();
         expect(pdoError.message).toEqual('');
+        const aggregate = new AggregateErrorFake([new Error('test'), new Error('test2')]);
+        pdoError = new PdoError(aggregate);
+        expect(pdoError.message).toEqual('test');
+        expect(pdoError.cause).toEqual(aggregate);
+        const aggregateWithMessage = new AggregateErrorFake([new Error('test'), new Error('test2')], 'Message');
+        pdoError = new PdoError(aggregateWithMessage);
+        expect(pdoError.message).toEqual('Message');
+        expect(pdoError.cause).toEqual(aggregateWithMessage);
     });
 });
--- src/errors/pdo-error.ts
+++ src/errors/pdo-error.ts
@@ -2,7 +2,15 @@ class PdoError extends Error {
     public cause: any;
 
     constructor(error: Error | string = '', cause?: any) {
-        super(typeof error === 'string' ? error : error.message);
+        super(
+            typeof error === 'string'
+                ? error
+                : // @ts-expect-error AggregateError
+                error.message === '' && error.errors != null && error.errors.length > 0
+                ? // @ts-expect-error AggregateError
+                  error.errors[0].message
+                : error.message
+        );
         if (cause !== undefined) {
             this.cause = cause;
         } else {
